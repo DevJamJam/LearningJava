8b581ed8351ffc7573c9b36f74afb734c0b280f6
SaGongSa <84135850+developer-jamjam@users.noreply.github.com>
2024-12-17 11:44:03 +0900

    Chapter_33 다형성
    실습 :
    final
    final 사용 시 상속 X , Override 사용 불가

--- src/exercise/chapter_33/Animal.java
+++ src/exercise/chapter_33/Animal.java
@@ -1,10 +1,12 @@
 package exercise.chapter_33;
 
 public class Animal {
+    // public과 class 사이에 final 작성 시 상속 불가하여 error 발생한다.
     // 속성
     protected String gender;
 
     // 행위
+    // public과 void사이에 final 작성 시 can not override  error 발생
     public void eat(String food) {
         System.out.printf("동물이 %s 을 먹습니다.\n", food);
     }
